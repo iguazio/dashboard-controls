b7276eb85d23559f6c5af069de308906cede5224
maksym.kyselytsia <maksym.kyselytsia@globallogic.com>
2018-06-12 15:55:55 +0300

    added `java` runtime attributes

--- src/nuclio/projects/project/functions/version/version-configuration/tabs/version-configuration-runtime-attributes/version-configuration-runtime-attributes.component.js
+++ src/nuclio/projects/project/functions/version/version-configuration/tabs/version-configuration-runtime-attributes/version-configuration-runtime-attributes.component.js
@@ -4,15 +4,49 @@
     angular.module('iguazio.dashboard-controls')
         .component('nclVersionConfigurationRuntimeAttributes', {
             bindings: {
-                version: '<'
+                version: '<',
+                onChangeCallback: '<'
             },
             templateUrl: 'nuclio/projects/project/functions/version/version-configuration/tabs/version-configuration-runtime-attributes/version-configuration-runtime-attributes.tpl.html',
             controller: NclVersionConfigurationRuntimeAttributesController
         });
 
-    function NclVersionConfigurationRuntimeAttributesController() {
+    function NclVersionConfigurationRuntimeAttributesController(lodash) {
         var ctrl = this;
 
-        // TODO
+        ctrl.$onInit = onInit;
+        ctrl.inputValueCallback = inputValueCallback;
+
+        //
+        // Hook method
+        //
+
+        /**
+         * Initialization method
+         */
+        function onInit() {
+            ctrl.runtimeAttributes = lodash.get(ctrl.version, 'spec.build.runtimeAttributes', []);
+
+            ctrl.runtimeAttributes = ctrl.runtimeAttributes.join('\n');
+        }
+
+        //
+        // Public method
+        //
+
+        /**
+         * Update spec.runtimeAttributes value
+         * @param {string} newData
+         * @param {string} field
+         */
+        function inputValueCallback(newData, field) {
+            if (field === 'attributes') {
+                ctrl.runtimeAttributes = newData;
+                ctrl.version.spec.build.runtimeAttributes = newData.replace('\r', '\n').split('\n');
+            } else {
+                lodash.set(ctrl.version, field, newData);
+            }
+            ctrl.onChangeCallback();
+        }
     }
 }());
