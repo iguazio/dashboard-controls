38024ec68972e1bff88ae2b6e6a3671a5ea870d5
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2018-08-08 16:18:35 +0300

    Fix [Nuclio] `spec.disabled` is used instead of `spec.disable`

--- src/nuclio/projects/project/functions/version/version-configuration/tabs/version-configuration-basic-settings/version-configuration-basic-settings.component.js
+++ src/nuclio/projects/project/functions/version/version-configuration/tabs/version-configuration-basic-settings/version-configuration-basic-settings.component.js
@@ -47,7 +47,7 @@
                 }
             }
 
-            ctrl.enableFunction = !lodash.get(ctrl.version, 'spec.disabled', false);
+            ctrl.enableFunction = !lodash.get(ctrl.version, 'spec.disable', false);
         }
 
         //
@@ -82,7 +82,7 @@
          * Switches enable/disable function status
          */
         function updateEnableStatus() {
-            lodash.set(ctrl.version, 'spec.disabled', !ctrl.enableFunction);
+            lodash.set(ctrl.version, 'spec.disable', !ctrl.enableFunction);
             ctrl.onChangeCallback();
         }
     }
