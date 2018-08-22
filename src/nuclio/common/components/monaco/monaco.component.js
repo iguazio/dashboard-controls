46c658d1983b1694b3dcd71fde10310e532d6c3f
LevkoKozak <levko.kozak@globallogic.com>
2018-08-22 13:34:56 +0300

    fixed issue with monaco content changes

--- src/nuclio/common/components/monaco/monaco.component.js
+++ src/nuclio/common/components/monaco/monaco.component.js
@@ -24,6 +24,7 @@
 
         ctrl.$onInit = onInit;
         ctrl.$onChanges = onChanges;
+        ctrl.onCodeChange = onCodeChange;
 
         //
         // Hook methods
@@ -37,17 +38,6 @@
                 language: ctrl.language,
                 code: ctrl.functionSourceCode
             };
-
-            $scope.$watch('selectedCodeFile.code', function () {
-                if (angular.isFunction(ctrl.onChangeSourceCodeCallback)) {
-                    ctrl.onChangeSourceCodeCallback({
-                        sourceCode: $scope.selectedCodeFile.code,
-                        language: $scope.selectedCodeFile.language
-                    });
-                }
-            });
-
-            $scope.$on('monaco_on-change-content', setNewSourceCode);
         }
 
         /**
@@ -68,17 +58,16 @@
         //
 
         /**
-         * Sets new code data such as source code and language
-         * @param {Event} event
-         * @param {Object} data
+         * On code change callback.
+         * igz-monaco-editor directive calls this callback with new changed content
+         * @param {string} newCode - changed code
          */
-        function setNewSourceCode(event, data) {
-            if (angular.isUndefined(data.name) || data.name === ctrl.name) {
-                ctrl.language = data.language;
-                $scope.selectedCodeFile = {
-                    language: data.language,
-                    code: data.code
-                };
+        function onCodeChange(newCode) {
+            if (angular.isFunction(ctrl.onChangeSourceCodeCallback)) {
+                ctrl.onChangeSourceCodeCallback({
+                    sourceCode: newCode,
+                    language: $scope.selectedCodeFile.language
+                });
             }
         }
     }
