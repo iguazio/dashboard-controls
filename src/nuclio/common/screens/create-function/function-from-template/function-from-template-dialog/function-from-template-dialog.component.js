db0f86763bf0dd63e0b2fd9d529f5d3d3e25b17c
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2018-12-30 19:44:10 +0200

    Impl [Nuclio] Function template params: order by `order` property
    Also:
    - Add "more info" icon with contents of `description` in case it is
      non-empty.
    - Add red asterisk in case `required` is `true`.
    - Limit dialog's height and add vertical scrollbar.
    - Skip params whose `kind` is not one of the defined
      available values (currently: `'string'`, `'number'` and `'choice'`).
    - `kind` is now case-INsensitive.
    - Ignore duplicate params by key. In case there are more than one params
      sharing the same key, then the first one will be used and the others
      will be skipped.
    - Bugfix: "Apply" button works although it looks disabled.

--- src/nuclio/common/screens/create-function/function-from-template/function-from-template-dialog/function-from-template-dialog.component.js
+++ src/nuclio/common/screens/create-function/function-from-template/function-from-template-dialog/function-from-template-dialog.component.js
@@ -14,17 +14,38 @@
     function NclFunctionFromTemplateDialogController(lodash, EventHelperService) {
         var ctrl = this;
 
+        var FILED_KINDS = ['string', 'number', 'choice'];
+
+        var defaultAttributes = {
+            string: {
+                defaultValue: '',
+                password: false
+            },
+            number: {
+                defaultValue: 0,
+                step: 1,
+                allowZero: false,
+                allowNegative: false,
+                allowDecimal: false
+            },
+            choice: {
+                choices: [],
+                defaultValue: '' // currently assuming "choice" to be a list of strings only
+            }
+        };
         var templateData = {};
 
         ctrl.dropdownOptions = {};
+        ctrl.fields = [];
+        ctrl.templateForm = null;
 
         ctrl.$onInit = onInit;
 
-        ctrl.onApply = onApply;
-        ctrl.onClose = onClose;
+        ctrl.dropdownCallback = dropdownCallback;
         ctrl.inputValueCallback = inputValueCallback;
         ctrl.isFormFilled = isFormFilled;
-        ctrl.dropdownCallback = dropdownCallback;
+        ctrl.onApply = onApply;
+        ctrl.onClose = onClose;
 
         //
         // Hook methods
@@ -34,16 +55,56 @@
          * Initialization method
          */
         function onInit() {
-            lodash.forIn(ctrl.template.values, function (value, key) {
-                var defaultValue = lodash.get(value, 'attributes.defaultValue', value.kind === 'number' ? 0 : '');
+            ctrl.fields = lodash.chain(ctrl.template.values)
+                .cloneDeep()
+                .forIn(function (field, key) {
+                    field.name = key;
+                })
+                .filter(function (field) {
+                    var kind = lodash.get(field, 'kind');
+                    return lodash.isString(kind) ? lodash.includes(FILED_KINDS, kind.toLowerCase()) : false;
+                })
+                .map(function (field) {
 
-                lodash.set(templateData, key, defaultValue);
+                    // converting `kind` property to lower-case in order to be more flexible, allowing the user to
+                    // specify kind in case-insensitive.
+                    field.kind = field.kind.toLowerCase();
 
-                if (value.kind === 'choice') {
-                    lodash.set(ctrl.dropdownOptions, key + '.options', prepareDropdownValue(lodash.get(value, 'attributes.choices')));
-                    lodash.set(ctrl.dropdownOptions, key + '.defaultValue', prepareDropdownValue(defaultValue));
-                }
-            });
+                    // setting default values to various properties
+                    lodash.defaults(field, {
+                        displayName: 'Unspecified field name',
+                        description: '',
+                        required: false,
+                        order: Infinity,
+                        attributes: lodash.defaults(field.attributes, defaultAttributes[field.kind])
+                    });
+
+                    if (field.kind === 'number') {
+                        if (!field.attributes.allowNegative && field.attributes.minValue < 0) {
+                            field.attributes.minValue = field.attributes.allowZero ? 0 : 1;
+                        }
+                    }
+
+                    if (field.kind === 'choice') {
+                        lodash.update(field, 'attributes.choices', function (choices) {
+                            return !lodash.isArray(choices) ? [] : lodash.map(choices, function (choice) {
+                                return lodash.isString(choice) ? {
+                                    id: choice,
+                                    name: choice,
+                                    visible: true
+                                } : choice;
+                            });
+                        });
+                    }
+
+                    return field;
+                })
+                .uniqBy('name') // prevent `ngRepeat` from breaking on duplicates.
+                .sortBy('order')
+                .forEach(function (field) {
+                    lodash.set(templateData, field.name, field.attributes.defaultValue);
+                })
+                .value();
         }
 
         //
@@ -65,8 +126,11 @@
          * @param {Event} [event]
          */
         function onApply(event) {
-            if ((angular.isUndefined(event) || event.keyCode === EventHelperService.ENTER) && !ctrl.isLoadingState) {
-                ctrl.closeDialog({template: templateData});
+            if (isFormFilled()) {
+                if ((angular.isUndefined(event) || event.keyCode === EventHelperService.ENTER) &&
+                    !ctrl.isLoadingState) {
+                    ctrl.closeDialog({ template: templateData });
+                }
             }
         }
 
@@ -95,29 +159,6 @@
         function dropdownCallback(newData, isChanged, field) {
             if (isChanged) {
                 lodash.set(templateData, field, newData.id);
-            }
-        }
-
-        //
-        // Private methods
-        //
-
-        /**
-         * Converts values for drop-down.
-         */
-        function prepareDropdownValue(value) {
-            if (lodash.isArray(value)) {
-                return lodash.map(value, function (option) {
-                    return {
-                        id: option,
-                        name: option
-                    };
-                });
-            } else if (lodash.isString(value)) {
-                return {
-                    id: value,
-                    name: value
-                }
             }
         }
     }
