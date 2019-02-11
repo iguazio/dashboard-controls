12993480180d2365d6244b8d54e740975d41af29
Andrii Mavdryk <andrii.mavdryk@globallogic.com>
2019-02-11 18:56:52 +0200

    IG-2112 [Services] Disable "Refresh" while applying changes

--- src/igz_controls/components/action-item/action-item-refresh/action-item-refresh.component.js
+++ src/igz_controls/components/action-item/action-item-refresh/action-item-refresh.component.js
@@ -4,6 +4,7 @@
     angular.module('iguazio.dashboard-controls')
         .component('igzActionItemRefresh', {
             bindings: {
+                isDisabled: '<',
                 refresh: '&'
             },
             templateUrl: 'igz_controls/components/action-item/action-item-refresh/action-item-refresh.tpl.html'
