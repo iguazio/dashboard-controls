f527a506d0472f35bc34279a860692a83ecf53cf
YarovoiVadym <37179001+YarovoiVadym@users.noreply.github.com>
2019-02-22 15:30:26 +0200

    Impl IG-2127 [General] Remove duplication for copy-to-clipboard (#457)

--- src/nuclio/projects/project/functions/version/version-code/function-event-pane/function-event-pane.component.js
+++ src/nuclio/projects/project/functions/version/version-code/function-event-pane/function-event-pane.component.js
@@ -140,7 +140,6 @@
 
         ctrl.addNewHeader = addNewHeader;
         ctrl.cancelInvocation = cancelInvocation;
-        ctrl.copyToClipboard = copyToClipboard;
         ctrl.downloadResponseFile = downloadResponseFile;
         ctrl.deleteEvent = deleteEvent;
         ctrl.deleteFile = deleteFile;
@@ -259,27 +258,6 @@
                 canceler = null;
             }
             canceledInvocation = true;
-        }
-
-        /**
-         * Copies a string to the clipboard. Must be called from within an event handler such as click
-         */
-        function copyToClipboard() {
-            if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
-                var textarea = document.createElement('textarea');
-                textarea.textContent = ctrl.testResult.body;
-                textarea.style.position = 'fixed';
-                document.body.appendChild(textarea);
-                textarea.select();
-
-                try {
-                    return document.execCommand('copy'); // Security exception may be thrown by some browsers.
-                } catch (ex) {
-                    DialogsService.alert('Copy to clipboard failed.', ex);
-                } finally {
-                    document.body.removeChild(textarea);
-                }
-            }
         }
 
         /**
