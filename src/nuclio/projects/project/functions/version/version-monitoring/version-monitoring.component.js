0d40a86f9356616991d2a0afbe92449c2dd4ea00
vadym.yarovoi <vadym.yarovoi@globallogic.com>
2019-02-28 11:26:14 +0200

    IG-2142 [Nuclio] Status tab: update contents while deploying (fixed)

--- src/nuclio/projects/project/functions/version/version-monitoring/version-monitoring.component.js
+++ src/nuclio/projects/project/functions/version/version-monitoring/version-monitoring.component.js
@@ -82,7 +82,7 @@
          * Sets actual deploying status in `ctrl.versionStatus`
          */
         function setVersionStatus() {
-            if (lodash.isEmpty(lodash.get(ctrl.version, 'ui.deployResult', {}))) {
+            if (lodash.isEmpty(lodash.get(ctrl.version, 'ui.deployResult'))) {
                 ctrl.versionStatus = lodash.get(ctrl.version, 'status');
             } else {
                 ctrl.versionStatus = lodash.get(ctrl.version, 'ui.deployResult.status');
