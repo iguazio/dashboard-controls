74f12166c1161ec646edbdd7e10819e8013e855a
YarovoiVadym <37179001+YarovoiVadym@users.noreply.github.com>
2019-04-22 18:42:13 +0300

    Impl IG-2225 [General] Make "More info" icon a common component (#555)

---
+++ src/igz_controls/components/more-info/more-info.component.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+
+    angular.module('iguazio.dashboard-controls')
+        .component('igzMoreInfo', {
+            bindings: {
+                description: '@',
+                isDisabled: '<?',
+                isDefaultTooltipEnabled: '<?',
+                defaultTooltipPlacement: '@?',
+                defaultTooltipPopupDelay: '@?'
+            },
+            templateUrl: 'igz_controls/components/more-info/more-info.tpl.html',
+            controller: IgzMoreInfoController
+        });
+
+    function IgzMoreInfoController(lodash) {
+        var ctrl = this;
+
+        ctrl.$onInit = onInit;
+
+        //
+        // Hook methods
+        //
+
+        /**
+         * Initialization method
+         */
+        function onInit() {
+            lodash.defaults(ctrl, {
+                isDisabled: false,
+                isDefaultTooltipEnabled: false,
+                defaultTooltipPlacement: 'auto',
+                defaultTooltipPopupDelay: '0'
+            });
+        }
+    }
+}());
