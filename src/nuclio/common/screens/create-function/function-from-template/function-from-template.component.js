990381e32cbb6ddd596138f089bae56f6236ba19
Vadym Yarovoi <37179001+YarovoiVadym@users.noreply.github.com>
2019-10-15 17:37:07 +0300

    Fix [Nuclio] Creating a new function from template not working (#751)

--- src/nuclio/common/screens/create-function/function-from-template/function-from-template.component.js
+++ src/nuclio/common/screens/create-function/function-from-template/function-from-template.component.js
@@ -123,7 +123,7 @@
                             if (!lodash.isNil(data.value)) {
                                 lodash.set(ctrl.functionData, 'values', data.value);
 
-                                ctrl.renderTemplate({ template: lodash.omit(ctrl.functionData, ['rendered', 'metadata']) })
+                                ctrl.renderTemplate({ template: lodash.omit(ctrl.functionData, ['rendered', 'metadata', 'ui']) })
                                     .then(function (response) {
                                         lodash.set(ctrl.functionData, 'rendered.spec', response.spec);
 
