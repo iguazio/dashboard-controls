9e11ec8c197109639315752a24455e487b7e3b26
larionovvS <stanislav.larionov@globallogic.com>
2020-01-14 16:09:53 +0200

    moved new project dialog to Iguazio dashboard and Nuclio open-source repo

--- src/nuclio/common/screens/create-function/create-function.component.js
+++ src/nuclio/common/screens/create-function/create-function.component.js
@@ -145,17 +145,7 @@
          * New project dialog
          */
         function createNewProject() {
-            ngDialog.open({
-                template: '<igz-new-project-dialog data-close-dialog="closeThisDialog(project)" ' +
-                          'data-create-project-callback="ngDialogData.createProject({project: project})"></igz-new-project-dialog>',
-                plain: true,
-                scope: $scope,
-                data: {
-                    createProject: ctrl.createProject
-                },
-                className: 'ngdialog-theme-nuclio'
-            })
-                .closePromise
+            ctrl.createProject()
                 .then(function (data) {
                     if (!lodash.isNil(data.value)) {
                         ctrl.isSplashShowed.value = true;
