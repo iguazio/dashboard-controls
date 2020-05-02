0bb1ac52d21706d9e5fc4bd78368c06281c50121
Eran Nussbaum <eran-nussbaum@users.noreply.github.com>
2020-05-02 16:51:55 +0300

    Impl [igzValidatePasswordConfirmation] Move component to this repo

---
+++ src/igz_controls/directives/validate-password-confirmation.directive.js
@@ -0,0 +1,55 @@
+(function () {
+    'use strict';
+
+    angular.module('iguazio.app')
+        .directive('igzValidatePasswordConfirmation', igzValidatePasswordConfirmation);
+
+    function igzValidatePasswordConfirmation(lodash) {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            scope: {
+                compareVal: '=igzValidatePasswordConfirmation'
+            },
+            link: link
+        };
+
+        function link(scope, element, attributes, ngModel) {
+            activate();
+
+            //
+            // Private methods
+            //
+
+            /**
+             * Constructor
+             */
+            function activate() {
+                initValidator();
+            }
+
+            /**
+             * Method to add validators
+             */
+            function initValidator() {
+                if (angular.isDefined(scope.compareVal)) {
+                    ngModel.$validators.validatePasswordConfirmation = isValueValid;
+
+                    scope.$watch('compareVal', function () {
+                        ngModel.$validate();
+                    });
+                }
+            }
+
+            /**
+             * Method checks if passed value is valid
+             * @param {string} value - current changed value
+             * @returns {boolean} return true if value is valid
+             */
+            function isValueValid(value) {
+                return lodash.isEmpty(value) || (value === scope.compareVal);
+            }
+        }
+    }
+}());
+
