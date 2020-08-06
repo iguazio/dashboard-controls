8e44d3088af62c7da8a8c8ce19d37cd9228a773e
Andrew Mavdryk <andrii.mavdryk@globallogic.com>
2020-08-06 19:13:47 +0300

    Fix [Data Clusters] Clicking on action in item menu opens info (#1065)

--- src/igz_controls/components/action-item/action-item.component.js
+++ src/igz_controls/components/action-item/action-item.component.js
@@ -100,6 +100,8 @@
                 if (angular.isFunction(ctrl.action.callback)) {
                     ctrl.action.callback(ctrl.action);
                 }
+
+                event.stopPropagation();
             }
         }
 
