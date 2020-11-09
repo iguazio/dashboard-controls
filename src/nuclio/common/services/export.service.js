097e6283d40cf3acf34c668b6b651ca7118368d2
Andrii Mavdryk <andrii.mavdryk@globallogic.com>
2020-11-09 16:14:54 +0200

    [Services] Authenticator: add connectors and token expiry

--- src/nuclio/common/services/export.service.js
+++ src/nuclio/common/services/export.service.js
@@ -4,7 +4,7 @@
     angular.module('iguazio.dashboard-controls')
         .factory('ExportService', ExportService);
 
-    function ExportService($i18next, $q, $timeout, $window, i18next, lodash, YAML, DialogsService) {
+    function ExportService($i18next, $q, $timeout, $window, i18next, lodash, YAML, DialogsService, YamlService) {
         return {
             exportFunction: exportFunction,
             getFunctionConfig: getFunctionConfig,
@@ -35,7 +35,7 @@
         function getFunctionConfig(version) {
             var functionConfig = prepareFunctionData(version);
 
-            return prepareYamlObject(functionConfig);
+            return YamlService.prepareYamlObject(functionConfig);
         }
 
         /**
@@ -102,7 +102,7 @@
                                 functions: lodash.defaultTo(functionsList, [])
                             }
                         };
-                    })
+                    });
             });
 
             $q.all(promises)
@@ -146,57 +146,6 @@
         }
 
         /**
-         * Returns valid YAML string.
-         * First RegExp deletes all excess lines in YAML string created by issue in yaml.js package.
-         * It is necessary to generate valid YAML.
-         * Example:
-         * -
-         *   name: name
-         *   value: value
-         * -
-         *   name: name
-         *   value: value
-         * Will transform in:
-         * - name: name
-         *   value: value
-         * - name: name
-         *   value: value
-         * Second and Third RegExp replaces all double quotes with single quotes outside the value.
-         * Example:
-         * 'key': "" -> 'key': ''
-         * 'key': "some "string" value" -> 'key': 'some "string" value'
-         * Fourth RegExp transform all double quotes to escaped double quotes inside the value.
-         * Example:
-         * 'key': 'some "string" value' -> 'key': 'some \"string\" value'
-         * Fifth, Sixth and Seventh replaces all single quotes with double quotes outside the value.
-         * Example:
-         * 'key': 'value' -> "key": "value"
-         * Eighth RegExp replaces all pairs of single quotes with one single quote.
-         * It needs because property name or property value is a string which contains single quote
-         * will parsed by yaml.js package in string with pair of single quotes.
-         * Example:
-         * "ke'y": "val'ue"
-         * After will parse will be -> "ke''y": "val''ue"
-         * This RegExp will transform it to normal view -> "ke'y": "val'ue"
-         * @param {string} data - incoming YAML-string
-         * @returns {string}
-         */
-        function getValidYaml(data) {
-            function replacer(match, captureGroup1, captureGroup2) {
-                return captureGroup1 + captureGroup2.replace(/"/g, '\\"')
-            }
-
-            return data.replace(/(\s+-)\s*\n\s+/g, '$1 ')
-                .replace(/(:\s)"(.+)"/g, '$1\'$2\'')
-                .replace(/(:\s)"{2}/g, '$1\'\'')
-                .replace(/([^\\"])("+)/g, replacer)
-                .replace(/'(.+)'(:)/g, '"$1"$2')
-                .replace(/(:\s)'(.+)'/g, '$1"$2"')
-                .replace(/(:\s)'{2}/g, '$1""')
-                .replace(/'{2}/g, '\'');
-        }
-
-        /**
          * Prepare function data
          * @param {Object} version
          * @returns {Object} data for export
@@ -218,22 +167,12 @@
         }
 
         /**
-         * Prepare YAML object
-         * @param {Object} objectToParse
-         * @returns {string} YAML object
-         */
-        function prepareYamlObject(objectToParse) {
-            var parsedObject = YAML.stringify(objectToParse, Infinity, 2);
-
-            return getValidYaml(parsedObject);
-        }
-        /**
          * Prepare blob object for downloading
          * @param {Object} objectToParse
          * @returns {Blob} Blob object
          */
         function prepareBlobObject(objectToParse) {
-            var parsedObject = prepareYamlObject(objectToParse);
+            var parsedObject = YamlService.prepareYamlObject(objectToParse);
 
             return new Blob([parsedObject], {
                 type: 'application/json'
