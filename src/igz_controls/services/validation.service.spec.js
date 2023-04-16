e6b0cfa13d628dd25d863c4300b4c5ff0168b377
Andrew Mavdryk <andrii.mavdryk@globallogic.com>
2023-04-16 20:52:44 +0300

    Fix [Identity] Can't find needed Users during User Group creation `3.5.x` (#1468)

--- src/igz_controls/services/validation.service.spec.js
+++ src/igz_controls/services/validation.service.spec.js
@@ -24,4 +24,75 @@ describe('ValidationService: ', function () {
             expect(result).toEqual(128);
         });
     });
+
+    describe('isValidByRules', () => {
+        it('should return true when given an empty rules array', () => {
+            expect(ValidationService.isValidByRules([], 'test')).toBe(true);
+        });
+
+        it('should return true when given a single rule that matches the value', () => {
+            var rules = [
+                {
+                    pattern: /^test$/,
+                },
+            ];
+            expect(ValidationService.isValidByRules(rules, 'test')).toBe(true);
+        });
+
+        it('should return false when given a single rule that does not match the value', () => {
+            var rules = [
+                {
+                    pattern: /^test$/,
+                },
+            ];
+            expect(ValidationService.isValidByRules(rules, 'notTest')).toBe(false);
+        });
+
+        it('should return true when given multiple rules that all match the value', () => {
+            var rules = [
+                {
+                    pattern: /^t/,
+                },
+                {
+                    pattern: /e/,
+                },
+                {
+                    pattern: /s/,
+                },
+                {
+                    pattern: /t$/,
+                },
+            ];
+            expect(ValidationService.isValidByRules(rules, 'test')).toBe(true);
+        });
+
+        it('should return false when given multiple rules and one does not match the value', () => {
+            var rules = [
+                {
+                    pattern: /^t/,
+                },
+                {
+                    pattern: /e/,
+                },
+                {
+                    pattern: /s/,
+                },
+                {
+                    pattern: /t$/,
+                },
+            ];
+            expect(ValidationService.isValidByRules(rules, 'notTest')).toBe(false);
+        });
+
+        it('should call the pattern function when given a function rule', () => {
+            var patternFn = jasmine.createSpy('patternFn').and.returnValue(true);
+            var rules = [
+                {
+                    pattern: patternFn,
+                },
+            ];
+            ValidationService.isValidByRules(rules, 'test');
+            expect(patternFn).toHaveBeenCalledWith('test');
+        });
+    });
 });
