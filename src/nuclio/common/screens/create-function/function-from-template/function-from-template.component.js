e95bcdc26f1c0c0158c612a35d291e6cee7bc7fe
illia-prokopchuk <78905712+illia-prokopchuk@users.noreply.github.com>
2025-03-26 15:04:58 +0200

    Revert "Impl [UI] Add Python 3.12 to Nuclio runtime dropdown `3.6.x` (#1633)" (#1636)
    This reverts commit c46e7a1c9ac8f8c64485587fd9e55e3e6e2e2cb4.

--- src/nuclio/common/screens/create-function/function-from-template/function-from-template.component.js
+++ src/nuclio/common/screens/create-function/function-from-template/function-from-template.component.js
@@ -404,12 +404,6 @@ such restriction.
                     visible: true
                 },
                 {
-                    id: 'python:3.12',
-                    name: 'Python 3.12',
-                    sourceCode: 'ZGVmIGhhbmRsZXIoY29udGV4dCwgZXZlbnQpOg0KICAgIHJldHVybiAiIg==', // source code in base64
-                    visible: true
-                },
-                {
                     id: 'dotnetcore',
                     name: '.NET Core ' + $i18next.t('functions:TECH_PREVIEW_LABEL', {lng: lng}),
                     visible: true
