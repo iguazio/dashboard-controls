40ba593923156b842fe3a5ecd4dd48c2e8c787f1
illia.prokopchuk <illia.prokopchuk@globallogic.com>
2026-02-05 13:33:46 +0200

    Revert "Impl [UI] Use Nuclio API to get function execution logs (#1654)"
    This reverts commit 6640d5934ea2aa57504dd145ebbf27ec12352465.

--- src/nuclio/functions/version/version-execution-log/version-execution-log.component.js
+++ src/nuclio/functions/version/version-execution-log/version-execution-log.component.js
@@ -28,12 +28,16 @@ such restriction.
             controller: NclVersionExecutionLogController
         });
 
-    function NclVersionExecutionLogController(lodash, moment, $interval, i18next, $i18next, $rootScope, ExecutionLogsDataService,
-                                              ExportService, LoginService, PaginationService) {
+    function NclVersionExecutionLogController(lodash, $interval, i18next, $i18next, $rootScope, moment, ControlPanelLogsDataService,
+                                              ConfigService, ExportService, LoginService, PaginationService) {
         var ctrl = this;
         var lng = i18next.language;
 
         var refreshInterval = null;
+        var initialTimeRange = {
+            from: null,
+            to: null
+        };
         var initialDatePreset = '7d';
         var initialReplicas = [];
         var defaultFilter = {
@@ -46,12 +50,12 @@ such restriction.
                 error: false
             }
         };
-        var projectName = '';
 
         ctrl.downloadButtonIsDisabled = false;
         ctrl.isSplashShowed = {
             value: false
         };
+        ctrl.lastEntryTimestamp = null;
         ctrl.logs = {};
         ctrl.replicasList = [];
         ctrl.filter = {};
@@ -65,11 +69,7 @@ such restriction.
         };
         ctrl.datePreset = initialDatePreset;
         ctrl.logsAreDownloading = false;
-        ctrl.timeRange = {
-            from: null,
-            to: null,
-            sort: 'desc'
-        };
+        ctrl.timeRange = initialTimeRange;
         ctrl.searchStates = {};
         ctrl.selectedReplicas = [];
         ctrl.isFiltersShowed = {
@@ -187,27 +187,11 @@ such restriction.
             defaultFilter.name = ctrl.version.metadata.name;
             ctrl.filter = lodash.cloneDeep(defaultFilter);
 
-            projectName = lodash.get(ctrl.version, ['metadata', 'labels', 'nuclio.io/project-name']);
+            PaginationService.addPagination(ctrl, 'logs', 'ControlPanelLogsDataService', onChangePageCallback, true);
 
-            PaginationService.addPagination(ctrl, 'logs', 'ExecutionLogsDataService', onChangePageCallback, true);
+            ctrl.page.size = ctrl.perPageValues[0].id;
 
-            ctrl.timeRange = getInitialTimeRange();
-
-            ctrl.isSplashShowed.value = true;
-            ExecutionLogsDataService.getReplicasList(projectName, ctrl.version.metadata.name, {timeFilter: ctrl.timeRange}).then(function (replicas) {
-                ctrl.replicasList = replicas.map(function (replica) {
-                    return {
-                        label: replica,
-                        id: replica,
-                        value: replica,
-                        checked: true
-                    }
-                });
-                ctrl.selectedReplicas = angular.copy(replicas);
-                initialReplicas = replicas;
-
-                applyFilters();
-            });
+            applyFilters();
         }
 
         /**
@@ -227,6 +211,7 @@ such restriction.
         function applyFilters() {
             stopAutoUpdate();
             calcActiveFilters();
+            generateFilterQuery();
             searchWithParams(0, ctrl.page.size);
         }
 
@@ -237,8 +222,7 @@ such restriction.
             stopAutoUpdate();
 
             ctrl.downloadButtonIsDisabled = true;
-
-            return ExecutionLogsDataService.collectLogs(queryParams())
+            return ControlPanelLogsDataService.collectLogs(queryParams())
                 .then(function (response) {
                     ExportService.exportLogs(response, ctrl.version.metadata.name);
                 }).finally(function () {
@@ -289,7 +273,8 @@ such restriction.
             ctrl.timeRange = lodash.mapValues(dateTimeRange, function (range) {
                 return new Date(range).toISOString();
             });
-            ctrl.timeRange.sort = 'desc';
+
+            ctrl.lastEntryTimestamp = null;
         }
 
         /**
@@ -298,30 +283,11 @@ such restriction.
         function refreshLogs() {
             startAutoUpdate();
 
-            ctrl.isSplashShowed.value = true
-
-            ExecutionLogsDataService.getReplicasList(projectName, ctrl.version.metadata.name, {timeFilter: ctrl.timeRange})
-                .then(function (replicas) {
-                    ctrl.replicasList = replicas.map(function (replica) {
-                        return {
-                            label: replica,
-                            id: replica,
-                            value: replica,
-                            checked: true
-                        }
-                    });
-
-                    initialReplicas = replicas;
-
-                    return ExecutionLogsDataService.logsPaginated(ctrl.page.number, ctrl.page.size, queryParams())
-                        .then(function (logs) {
-                            if (logs.length > 0) {
-                                ctrl.logs = lodash.cloneDeep(logs);
-                            }
-                        });
-                })
-                .finally(function () {
-                    ctrl.isSplashShowed.value = false;
+            ControlPanelLogsDataService.logsPaginated(ctrl.page.number, ctrl.page.size, queryParams())
+                .then(function (logs) {
+                    if (logs.length > 0) {
+                        ctrl.logs = lodash.cloneDeep(logs);
+                    }
                 });
         }
 
@@ -330,7 +296,7 @@ such restriction.
          */
         function resetFilters() {
             ctrl.applyIsDisabled = false;
-            ctrl.timeRange = getInitialTimeRange();
+            ctrl.timeRange = initialTimeRange;
             ctrl.datePreset = initialDatePreset;
             ctrl.selectedReplicas = initialReplicas;
 
@@ -370,7 +336,7 @@ such restriction.
                 return;
             }
 
-            ExecutionLogsDataService.logsPaginated(ctrl.page.number, ctrl.page.size, queryParams())
+            ControlPanelLogsDataService.logsPaginated(ctrl.page.number, ctrl.page.size, queryParamsAutoUpdate())
                 .then(function (logs) {
                     if (logs.length > 0) {
                         ctrl.logs = lodash.cloneDeep(logs);
@@ -399,7 +365,54 @@ such restriction.
          * Updates latest timestamp when new data received and generates replicas list
          */
         function onChangePageCallback() {
+            if (ctrl.logs.length > 0) {
+                ctrl.lastEntryTimestamp = ctrl.logs[0].time;
+
+                if (ctrl.logs.replicas) {
+                    ctrl.replicasList = ctrl.logs.replicas.map(function (replica) {
+                        return {
+                            label: replica,
+                            id: replica,
+                            value: replica,
+                            checked: true
+                        }
+                    });
+                    ctrl.selectedReplicas = angular.copy(ctrl.logs.replicas);
+                    initialReplicas = ctrl.logs.replicas;
+                }
+            }
+
             startAutoUpdate();
+        }
+
+        /**
+         * Generates query string from all filters
+         */
+        function generateFilterQuery() {
+            var levels = lodash.chain(ctrl.filter.level).pickBy().keys().join(' OR ').value();
+            var projectName = lodash.get(ctrl.version, ['metadata', 'labels', 'nuclio.io/project-name']);
+            var projectFilter = '(nuclio.project_name.keyword:' + projectName + ' OR nuclio.project_name:' + projectName + ')';
+            var queries = ['system-id:"' + ConfigService.systemId + '"', '_exists_:nuclio', projectFilter];
+
+            if (ctrl.selectedReplicas.length && ctrl.selectedReplicas.length !== initialReplicas.length) {
+                var replicas = ctrl.selectedReplicas.join(' OR ');
+
+                queries.push('kubernetes.pod.name:(' + replicas + ')');
+            }
+
+            if (!lodash.isEmpty(ctrl.filter.message)) {
+                // Escape reserved characters: + - = && || > < ! ( ) { } [ ] ^ " ~ * ? : \ /
+                var escapedMessage = ctrl.filter.message.replace(/[+\-=&|!(){}[\]^"~*?:\\/]/g, '\\$&')
+                    .replace(/<|>/g, '');
+                queries.push('(message:' + escapedMessage + ' OR more:' + escapedMessage + ')');
+            }
+
+            if (!lodash.isEmpty(levels)) {
+                queries.push('level:(' + levels + ')');
+            }
+
+            ctrl.filterQuery = lodash.join(queries, ' AND ');
+            ctrl.filterName = ctrl.version.metadata.name;
         }
 
         /**
@@ -408,16 +421,21 @@ such restriction.
          */
         function queryParams() {
             return {
-                functionName: ctrl.version.metadata.name,
-                projectName,
-                filters: {
-                    substring: ctrl.filter.message.replace(/[+\-=&|!(){}[\]^"~*?:\\/]/g, '\\$&')
-                        .replace(/<|>/g, ''),
-                    logLevels: lodash.chain(ctrl.filter.level).pickBy().keys().value(),
-                    replicaNames: ctrl.selectedReplicas,
-                    timeFilter: ctrl.timeRange
-                }
+                query: ctrl.filterQuery,
+                filterName: ctrl.filterName,
+                timeFrame: ctrl.datePreset,
+                customTimeFrame: ctrl.timeRange
             };
+        }
+
+        /**
+         * Generates query params for auto update request
+         * @returns {Object}
+         */
+        function queryParamsAutoUpdate() {
+            return lodash.defaults(queryParams(), {
+                lastEntryTimestamp: ctrl.lastEntryTimestamp
+            });
         }
 
         /**
@@ -447,18 +465,6 @@ such restriction.
                 $interval.cancel(refreshInterval);
                 refreshInterval = null;
             }
-        }
-
-        /**
-         * Provides initial time range
-         */
-        function getInitialTimeRange() {
-            var weekDate = ctrl.customDatePresets['7d'].getRange();
-
-            return {
-                from: weekDate.from.toISOString(),
-                sort: 'desc'
-            };
         }
     }
 }());
